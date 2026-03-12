10a090c1f749826cb737edb3a2821775dfa2c85e
Quan Nguyen <qntrng94@gmail.com>
2026-03-12 11:57:01 +0100

    complete delete button

--- src/main.js
+++ src/main.js
@@ -18,8 +18,14 @@ loadNewJokeBtn.addEventListener("click", loadNewJoke);
 function saveJoke() {
   const jokeText = currentJokeEl.innerText;
   const witze = JSON.parse(localStorage.getItem("witze")) || [];
+
+  if (witze.includes(jokeText)) {
+    alert("Witz bereits gespeichert");
+    return;
+  }
   witze.push(jokeText);
   localStorage.setItem("witze", JSON.stringify(witze));
+  renderJokes();
 }
 
 saveJokeBtn.addEventListener("click", saveJoke);
@@ -34,8 +40,22 @@ function renderJokes() {
     item.classList.add("save-joke__item");
     item.innerHTML = `
       <div class="save-joke">${witz}</div>
-      <button class="saved-joke__remove">Löschen</button>
+      <button class="saved-joke__remove">
+      <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="saved-joke__remove-icon">
+      <path stroke-linecap="round" stroke-linejoin="round" d="m3 3 1.664 1.664M21 21l-1.5-1.5m-5.485-1.242L12 17.25 4.5 21V8.742m.164-4.078a2.15 2.15 0 0 1 1.743-1.342 48.507 48.507 0 0 1 11.186 0c1.1.128 1.907 1.077 1.907 2.185V19.5M4.664 4.664 19.5 19.5" />
+    </svg></button>
     `;
     witzeListEl.appendChild(item);
+
+    const deleteBtn = item.querySelector(".saved-joke__remove");
+    deleteBtn.addEventListener("click", () => deleteJoke(index));
   });
+}
+renderJokes();
+
+function deleteJoke(index) {
+  const witze = JSON.parse(localStorage.getItem("witze")) || [];
+  witze.splice(index, 1);
+  localStorage.setItem("witze", JSON.stringify(witze));
+  renderJokes();
 }
